b214aef7bb65baf6e2aeafe85813ba5438435168
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2025-07-03 15:17:39 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import usersRoutes from './src/users.routes'
+
+const app = express()
+
+app.get('/api', usersRoutes)
+
+app.listen(3000, () => {
+    console.log('Running!')
+})
