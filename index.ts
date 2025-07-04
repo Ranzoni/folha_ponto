95466018b189135ae6982091ebe0dd6413368e86
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2025-07-04 20:15:27 -0300

    Criação de cargo com comunicação com o banco de dados.

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import usersRoutes from './src/routes/users.routes'
+import positionsRoutes from './src/routes/positions.routes'
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/api', usersRoutes)
+app.use('/api', positionsRoutes)
 
 app.listen(3000, () => {
     console.log('Running!')
